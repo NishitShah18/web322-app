09e069354273aa02fcfdb57020d0a09abdae683c
Nishit Shah <ngshah3@myseneca.ca>
2022-09-26 14:56:05 -0400

    done with part - 1 and citations

--- public/code.js
+++ public/code.js
@@ -1,1 +1,13 @@
-// doing nothing :)
+/*********************************************************************************
+ *  WEB322 – Assignment 2
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ *
+ *  Name: Nishit Shah Student ID: 130 176 217 Date: 26th Sept 2022
+ *
+ *  Online (Cyclic) URL:
+ *
+ ********************************************************************************/
+
+// emptyy
