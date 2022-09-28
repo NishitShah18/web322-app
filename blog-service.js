f726a69be0430fb8c66e0c85f29f475dfeb819ea
Nishit Shah <ngshah3@myseneca.ca>
2022-09-28 18:05:27 -0400

    created all functions

--- blog-service.js
+++ blog-service.js
@@ -10,8 +10,72 @@
  *
  ********************************************************************************/
 
+const { rejects } = require("assert");
 const fs = require("fs"); // required at the top of my module
+const { resolve } = require("path");
 
 //Module Data
 var posts = [];
 var categories = [];
+
+//initialize()
+//•	This function will read the contents of the "./data/posts.json" and "./data/categories.json" file
+
+module.exports.initialize = function () {
+  return new Promise((res, rej) => {
+    fs.readFile("./data/posts.json"),
+      (err, data) => {
+        if (err) {
+          rej("unable to read file");
+        } else {
+          posts = JSON.parse(data);
+        }
+      };
+    fs.readFile("./data/categories.json"),
+      (err, data) => {
+        if (err) {
+          rej("unable to read file");
+        } else {
+          categories = JSON.parse(data);
+        }
+      };
+    res();
+  });
+};
+
+module.exports.getAllPosts = function () {
+  return new Promise((res, rej) => {
+    if (posts.length === 0) {
+      rej("no results returned");
+    } else {
+      res(posts);
+    }
+  });
+};
+
+module.exports.getPublishedPosts = function () {
+  return new Promise((res, rej) => {
+    var filteredPosts = [];
+    for (let i = 0; i < movies.length; i++) {
+      if (posts[i].published === true) {
+        filteredPosts.push(posts[i]);
+      }
+    }
+
+    if (filteredPosts.length === 0) {
+      rej("no results returned");
+    } else {
+      res(filteredPosts);
+    }
+  });
+};
+
+module.exports.getCategories = function () {
+  return new Promise((res, rej) => {
+    if (categories.length === 0) {
+      rej("no results returned");
+    } else {
+      res(categories);
+    }
+  });
+};
